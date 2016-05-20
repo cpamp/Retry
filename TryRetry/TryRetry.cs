14400be0570b2e9e6a8ce0e67c2c03226e86a2a6
Pamperin, Clint A <c.pamperin21@gmail.com>
2016-05-19 23:10:40 -0500

    First Commit

---
+++ TryRetry/TryRetry.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryRetry
+{
+    /// <summary>
+    /// Class for simulating Try, Catch, Retry (n) times.
+    /// </summary>
+    /// <typeparam name="TResult">Type to be returned.</typeparam>
+    public static class TryRetry<TResult>
+    {
+        /// <summary>
+        /// Try, catch, then retry (n) times until max tries reached, an unexpected exception is thrown, 
+        /// or try block executes without an exception.
+        /// </summary>
+        /// <typeparam name="TException">Expected <see cref="Exception"/> to handle.</typeparam>
+        /// <param name="tryFunc">Try code block to execute.</param>
+        /// <param name="catchFunc">Catch code block to execute.</param>
+        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
+        /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
+        /// <returns>tryFunc return value or catchFunc return value.</returns>
+        public static TResult Retry<TException>(Func<TResult> tryFunc, Func<TResult> catchFunc = null, 
+            int maxTries = 1, int millisecondsDelay = 0) where TException : Exception
+        {
+            TResult result = default(TResult);
+            int numTries = 0;
+            maxTries = maxTries < 0 ? 1 : maxTries;
+
+            while (numTries <= maxTries)
+            {
+                try
+                {
+                    result = tryFunc();
+                    break;
+                }
+                catch (TException)
+                {
+                    result = catchFunc();
+                }
+                finally
+                {
+                    numTries++;
+                    if (millisecondsDelay > 0 && numTries < maxTries)
+                        System.Threading.Thread.Sleep(millisecondsDelay);
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TryRetryTest/Test.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using TryRetry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TryRetryTest
+{
+    [TestClass]
+    public class Test
+    {
+        public TestContext TestContext { get; set; }
+
+        /// <summary>
+        /// Loop to test. Throws <see cref="IndexOutOfRangeException"/> on fail.
+        /// </summary>
+        /// <param name="fail">Whether the loop should throw <see cref="IndexOutOfRangeException"/> or not.</param>
+        /// <returns>Returns 0 on pass.</returns>
+        private int looper(bool fail = true)
+        {
+            TestContext.WriteLine("Looper");
+            int[] arr = { 1, 2 };
+            for (int i = 0; fail; i++)
+            {
+                int j = arr[i];
+            }
+            return 0;
+        }
+
+        /// <summary>
+        /// Method for catching exceptions.
+        /// </summary>
+        /// <returns>Returns 1 on catch.</returns>
+        private int catcher()
+        {
+            TestContext.WriteLine("Catcher");
+            return 1;
+        }
+
+        /// <summary>
+        /// Test for the wrong exception.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.IndexOutOfRangeException))]
+        public void WrongException()
+        {
+            TryRetry<int>.Retry<NullReferenceException>(
+                () => looper(),
+                catcher);
+        }
+
+        /// <summary>
+        /// Test for the right exception.
+        /// </summary>
+        [TestMethod]
+        public void RightException()
+        {
+            int result = TryRetry<int>.Retry<IndexOutOfRangeException>(
+                () => looper(),
+                catcher);
+        }
+
+        /// <summary>
+        /// Test for a failed attempt despite retrying.
+        /// </summary>
+        [TestMethod]
+        public void Failed()
+        {
+            int result = TryRetry<int>.Retry<IndexOutOfRangeException>(
+                () => looper(),
+                catcher);
+            Assert.AreEqual<int>(1, result);
+        }
+
+        /// <summary>
+        /// Test for a passed attempt without retrying.
+        /// </summary>
+        [TestMethod]
+        public void Passed()
+        {
+            int result = TryRetry<int>.Retry<IndexOutOfRangeException>(
+                () => looper(false),
+                catcher);
+            Assert.AreEqual<int>(0, result);
+        }
+    }
+}
