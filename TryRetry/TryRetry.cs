9bb2f2326657b4419838e88757c89414af449df3
Pamperin, Clint A <c.pamperin21@gmail.com>
2016-05-21 22:52:15 -0500

    Feature, handle multiple exceptions
    Retry can handle multiple exceptions with their own catch delegate using a dictionary of Type key and Func<T> value

--- TryRetry/TryRetry.cs
+++ TryRetry/TryRetry.cs
@@ -22,29 +22,64 @@ namespace TryRetry
         /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
         /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
         /// <returns>tryFunc return value or catchFunc return value.</returns>
-        public static TResult Retry<TException>(Func<TResult> tryFunc, Func<TResult> catchFunc = null, 
-            int maxTries = 1, int millisecondsDelay = 0) where TException : Exception
+        public static TResult Retry<TException>(Func<TResult> tryFunc, Func<TResult> catchFunc = null,
+            int maxTries = 1, int millisecondsDelay = 0) where TException : Exception, new()
+        {
+            return Retry(
+                tryFunc,
+                new Dictionary<Type, Func<TResult>>(){ { new TException().GetType(), catchFunc } },
+                maxTries,
+                millisecondsDelay);
+        }
+
+        /// <summary>
+        /// Try, catch, then retry (n) times until max tries reached, an unexpected exception is thrown, 
+        /// or try block executes without an exception.
+        /// </summary>
+        /// <typeparam name="TException">Expected <see cref="Exception"/> to handle.</typeparam>
+        /// <param name="tryFunc">Try code block to execute.</param>
+        /// <param name="exceptionCatch">Dictionary containing expected <see cref="Exception"/> <see cref="Type"/> 
+        /// as key and <see cref="Func{TResult}"/> to invoke for that <see cref="Exception"/> as value.</param>
+        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
+        /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
+        /// <returns>tryFunc return value or catchFunc return value.</returns>
+        public static TResult Retry(Func<TResult> tryFunc, IDictionary<Type, Func<TResult>> exceptionCatch,
+            int maxTries = 1, int millisecondsDelay = 0)
         {
             TResult result = default(TResult);
+            bool handled = false;
             int numTries = 0;
             maxTries = maxTries < 0 ? 1 : maxTries;
 
             while (numTries <= maxTries)
             {
+                handled = false;
                 try
                 {
                     result = tryFunc();
                     break;
                 }
-                catch (TException)
+                catch (Exception e)
                 {
-                    if (catchFunc != null) result = catchFunc();
+                    foreach (var ec in exceptionCatch)
+                    {
+                        if (e.GetType() == ec.Key)
+                        {
+                            if (ec.Value != null) { result = ec.Value.Invoke(); }
+                            handled = true;
+                            break;
+                        }
+                    }
+
+                    if (!handled) throw e;
                 }
                 finally
                 {
                     numTries++;
                     if (millisecondsDelay > 0 && numTries <= maxTries)
+                    {
                         System.Threading.Thread.Sleep(millisecondsDelay);
+                    }
                 }
             }
 
