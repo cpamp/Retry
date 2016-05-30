88d370d8b61b4622e5e8a12b4e265eb0833ddc57
Pamperin, Clint A <c.pamperin21@gmail.com>
2016-05-30 17:40:04 -0500

    Refactor, routines and constructors
    Routines no longer take parameters and now class must be built with constructor parameters.

--- Retry/Retry.cs
+++ Retry/Retry.cs
@@ -16,37 +16,123 @@ namespace Retry
         /// </summary>
         /// <param name="e">Exception that is being handled.</param>
         /// <returns>TResult</returns>
-        public delegate TResult CatchFunc(Exception e);
+        public delegate TResult CatchFunction(Exception e);
+
+        /// <summary>
+        /// Function to try.
+        /// </summary>
+        private Func<TResult> tryFunction;
+
+        /// <summary>
+        /// Exceptions to catch and their catching functions.
+        /// </summary>
+        private IDictionary<Type, CatchFunction> exceptionCatchFunctions;
+
+        /// <summary>
+        /// Id of this retry instance.
+        /// </summary>
+        private string retryId;
 
         /// <summary>
         /// The circuit breaker
         /// </summary>
-        private CircuitBreaker<TResult> cb;
+        private CircuitBreaker<TResult> circuitBreaker;
         
         /// <summary>
         /// Collection of results from run once runs.
         /// </summary>
         public static RunResults<TResult> Results = new RunResults<TResult>();
 
+        #region Constructors
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tryFunc">Try code block to execute.</param>
+        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
+        /// as key and <see cref="CatchFunction"/> to invoke for that <see cref="Exception"/> as value.</param>
+        public Retry(Func<TResult> tryFunc, IDictionary<Type, CatchFunction> exCatch) :
+            this(tryFunc, exCatch, 1, 0, 0, null)
+        { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tryFunc">Try code block to execute.</param>
+        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
+        /// as key and <see cref="CatchFunction"/> to invoke for that <see cref="Exception"/> as value.</param>
+        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
+        public Retry(Func<TResult> tryFunc, IDictionary<Type, CatchFunction> exCatch,
+            int maxTries) :
+            this(tryFunc, exCatch, maxTries, 0, 0, null)
+        { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tryFunc">Try code block to execute.</param>
+        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
+        /// as key and <see cref="CatchFunction"/> to invoke for that <see cref="Exception"/> as value.</param>
+        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
+        /// <param name="millisecondsToWait">Milliseconds to delay next try.</param>
+        public Retry(Func<TResult> tryFunc, IDictionary<Type, CatchFunction> exCatch,
+            int maxTries, int millisecondsToWait) :
+            this(tryFunc, exCatch, maxTries, millisecondsToWait, 0, null)
+        { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tryFunc">Try code block to execute.</param>
+        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
+        /// as key and <see cref="CatchFunction"/> to invoke for that <see cref="Exception"/> as value.</param>
+        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
+        /// <param name="millisecondsToWait">Milliseconds to delay next try.</param>
+        /// <param name="maxWaits">Maximum times to wait.</param>
+        public Retry(Func<TResult> tryFunc, IDictionary<Type, CatchFunction> exCatch,
+            int maxTries, int millisecondsToWait, int maxWaits) :
+            this(tryFunc, exCatch, maxTries, millisecondsToWait, maxWaits, null)
+        { }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="tryFunc">Try code block to execute.</param>
+        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
+        /// as key and <see cref="CatchFunction"/> to invoke for that <see cref="Exception"/> as value.</param>
+        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
+        /// <param name="millisecondsToWait">Milliseconds to delay next try.</param>
+        /// <param name="maxWaits">Maximum times to wait.</param>
+        /// <param name="id">Unique id of to associate with this call.</param>
+        public Retry(Func<TResult> tryFunc, IDictionary<Type, CatchFunction> exCatch,
+            int maxTries, int millisecondsToWait, int maxWaits, string id)
+        {
+            retryId = id;
+            tryFunction = tryFunc;
+            exceptionCatchFunctions = exCatch;
+            maxTries = Math.Max(maxTries, 1);
+            circuitBreaker = new CircuitBreaker<TResult>(maxTries, millisecondsToWait, maxWaits);
+        }
+        #endregion
+
         /// <summary>
         /// Handles exceptions thrown
         /// </summary>
         /// <param name="e">The <see cref="Exception"/> thrown.</param>
         /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
-        /// as key and <see cref="CatchFunc"/> to invoke for that <see cref="Exception"/> as value.</param>
+        /// as key and <see cref="CatchFunction"/> to invoke for that <see cref="Exception"/> as value.</param>
         /// <returns>Result of catch function.</returns>
-        private TResult HandleException(Exception e, IDictionary<Type, CatchFunc> exCatch)
+        private TResult HandleException(Exception e)
         {
             TResult result = default(TResult);
             bool handled = false;
 
-            foreach (var ec in exCatch)
+            foreach (var ec in exceptionCatchFunctions)
             {
                 if (e.GetType() == ec.Key)
                 {
                     if (ec.Value != null)
                     {
-                        CatchFunc catchFunc = ec.Value;
+                        CatchFunction catchFunc = ec.Value;
                         result = catchFunc(e);
                     }
                     handled = true;
@@ -62,25 +148,18 @@ namespace Retry
         /// Try, catch, then retry (n) times until max tries reached, an unexpected exception is thrown, 
         /// or try block executes without an exception.
         /// </summary>
-        /// <param name="tryFunc">Try code block to execute.</param>
-        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
-        /// as key and <see cref="CatchFunc"/> to invoke for that <see cref="Exception"/> as value.</param>
-        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
-        /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
-        /// <returns>tryFunc return value or catchFunc return value.</returns>
-        private TResult RunRetry(Func<TResult> tryFunc, IDictionary<Type, CatchFunc> exCatch,
-            int maxTries = 1, int millisecondsDelay = 0, int halfOpenThreshold = -1)
+        /// <returns><see cref="TResult"/></returns>
+        private TResult RunRetry()
         {
             TResult result = default(TResult);
-            maxTries = Math.Max(maxTries, 1);
 
-            cb = new CircuitBreaker<TResult>(maxTries, millisecondsDelay, halfOpenThreshold);
+            circuitBreaker.Reset();
 
-            while (cb.Running)
+            while (circuitBreaker.Running)
             {
                 try
                 {
-                    result = cb.Execute(tryFunc);
+                    result = circuitBreaker.Execute(tryFunction);
                 }
                 catch (OpenCircuitException)
                 {
@@ -88,7 +167,7 @@ namespace Retry
                 }
                 catch (Exception e)
                 {
-                    result = HandleException(e, exCatch);
+                    result = HandleException(e);
                 }
             }
 
@@ -99,58 +178,18 @@ namespace Retry
         /// Try, catch, then retry (n) times until max tries reached, an unexpected exception is thrown, 
         /// or try block executes without an exception.
         /// </summary>
-        /// <typeparam name="TException">Expected <see cref="Exception"/> to handle.</typeparam>
-        /// <param name="tryFunc">Try code block to execute.</param>
-        /// <param name="catchFunc">Catch code block to execute.</param>
-        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
-        /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
-        /// <param name="id">Unique id of to associate with this call.</param>
-        /// <returns>tryFunc return value or catchFunc return value.</returns>
-        public TResult Run<TException>(Func<TResult> tryFunc, CatchFunc catchFunc = null,
-            int maxTries = 1, int millisecondsDelay = 0, string id = null) where TException : Exception
+        /// <returns><see cref="TResult"/></returns>
+        public TResult Run()
         {
             TResult result = default(TResult);
 
-            if (Results.CanRun(id))
+            if (Results.CanRun(retryId))
             {
-                result = RunRetry(
-                tryFunc,
-                new Dictionary<Type, CatchFunc>() { { typeof(TException), catchFunc } },
-                maxTries,
-                millisecondsDelay);
-                Results.AddResult(id, result);
+                result = RunRetry();
             }
             else
             {
-                result = Results.GetResult(id);
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Try, catch, then retry (n) times until max tries reached, an unexpected exception is thrown, 
-        /// or try block executes without an exception.
-        /// </summary>
-        /// <param name="tryFunc">Try code block to execute.</param>
-        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
-        /// as key and <see cref="CatchFunc"/> to invoke for that <see cref="Exception"/> as value.</param>
-        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
-        /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
-        /// <param name="id">Unique id of to associate with this call.</param>
-        /// <returns>tryFunc return value or catchFunc return value.</returns>
-        public TResult Run(Func<TResult> tryFunc, IDictionary<Type, CatchFunc> exCatch,
-            int maxTries = 1, int millisecondsDelay = 0, string id = null)
-        {
-            TResult result = default(TResult);
-
-            if (Results.CanRun(id))
-            {
-                result = RunRetry(tryFunc, exCatch, maxTries, millisecondsDelay);
-            }
-            else
-            {
-                result = Results.GetResult(id);
+                result = Results.GetResult(retryId);
             }
 
             return result;
@@ -160,64 +199,18 @@ namespace Retry
         /// Try, catch, then retry (n) times asynchronously until max tries reached, an unexpected exception is thrown, 
         /// or try block executes without an exception.
         /// </summary>
-        /// <param name="tryFunc">Try code block to execute.</param>
-        /// <param name="catchFunc">Catch code block to execute.</param>
-        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
-        /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
-        /// <param name="id">Unique id of to associate with this call.</param>
-        /// <param name="halfOpenThreshold">Number of times HalfOpen attempts are allowed to fail.</param>
-        /// <returns>Task</returns>
-        public async Task<TResult> RunAsync<TException>(Func<TResult> tryFunc, CatchFunc catchFunc = null,
-            int maxTries = 1, int millisecondsDelay = 0, string id = null, int halfOpenThreshold = -1) where TException : Exception
+        /// <returns>Task<<see cref="TResult"/>></returns>
+        public async Task<TResult> RunAsync()
         {
             TResult result = default(TResult);
 
-            if (Results.CanRun(id))
+            if (Results.CanRun(retryId))
             {
-                result = await Task.Run(() => RunRetry(
-                tryFunc,
-                new Dictionary<Type, CatchFunc>() { { typeof(TException), catchFunc } },
-                maxTries,
-                millisecondsDelay,
-                halfOpenThreshold));
+                result = await Task.Run(() => RunRetry());
             }
             else
             {
-                result = Results.GetResult(id);
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Try, catch, then retry (n) times asynchronously until max tries reached, an unexpected exception is thrown, 
-        /// or try block executes without an exception.
-        /// </summary>
-        /// <param name="tryFunc">Try code block to execute.</param>
-        /// <param name="exCatch"><see cref="IDictionary{TKey, TValue}"/> containing expected <see cref="Exception"/> <see cref="Type"/> 
-        /// as key and <see cref="Func{TResult}"/> to invoke for that <see cref="Exception"/> as value.</param>
-        /// <param name="maxTries">Maximum number of times to retry, minimum once.</param>
-        /// <param name="millisecondsDelay">Milliseconds to delay next try.</param>
-        /// <param name="id">Unique id of to associate with this call.</param>
-        /// <param name="halfOpenThreshold">Number of times HalfOpen attempts are allowed to fail.</param>
-        /// <returns>Task</returns>
-        public async Task<TResult> RunAsync(Func<TResult> tryFunc, IDictionary<Type, CatchFunc> exCatch,
-            int maxTries = 1, int millisecondsDelay = 0, string id = null, int halfOpenThreshold = -1)
-        {
-            TResult result = default(TResult);
-
-            if (Results.CanRun(id))
-            {
-                result = await Task.Run(() => RunRetry(
-                    tryFunc, 
-                    exCatch, 
-                    maxTries,
-                    millisecondsDelay, 
-                    halfOpenThreshold));
-            }
-            else
-            {
-                result = Results.GetResult(id);
+                result = Results.GetResult(retryId);
             }
 
             return result;
